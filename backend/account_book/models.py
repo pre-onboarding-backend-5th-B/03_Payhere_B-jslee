09f829a7e0bae21ce863c4815577e495ed43e8e1
Jong012 <Jong012>
2022-11-04 23:33:29 +0900

    feat(account.models): User 모델 import 수정

--- backend/account_book/models.py
+++ backend/account_book/models.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
+User = get_user_model()
 
 
 class AccountBook(models.Model):
