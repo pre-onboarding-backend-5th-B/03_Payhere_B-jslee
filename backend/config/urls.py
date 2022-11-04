388271dd52e2cec5b7a6b5559e8df172aff91c1e
Jong012 <44486924+Jong012@users.noreply.github.com>
2022-11-04 14:01:21 +0900

    Feature/user 앱 기능 구현 (#1)
    1. 회원가입 기능 구현
    2. 로그인 기능 구현

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="PayHere API",
         default_version='v1',
-        description="Test description",
+        description="PayHere AccountBook API",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-
+    path('user/', include('user.urls')),
     # swagger
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
