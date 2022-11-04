8c098c3fd96189b598ee35c52290776a65a80505
Jong012 <Jong012>
2022-11-04 13:42:36 +0900

    feat(user.urls): jwt url 추가

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
 from .views import SignupView
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup')
+    path('signup/', SignupView.as_view(), name='signup'),
+    re_path(r'^jwt-auth/', obtain_jwt_token),
+    re_path(r'^jwt-verify/', verify_jwt_token),
+    re_path(r'^jwt-refresh/', refresh_jwt_token),
 ]
