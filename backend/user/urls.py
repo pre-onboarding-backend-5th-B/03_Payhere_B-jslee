9fd1c14f8ceee7645c35576b346219025c9da309
Jong012 <44486924+Jong012@users.noreply.github.com>
2022-11-04 14:10:41 +0900

    UserApp 구현 완료 (#2)
    * feat(user): User 회원가입 및 로그인 기능 구현
    
    1. 회원가입 기능 구현
    2. 로그인 기능 구현

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
+from django.urls import path, re_path
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+
+from .views import SignupView
 
 urlpatterns = [
-
+    path('signup/', SignupView.as_view(), name='signup'),
+    re_path(r'^jwt-auth/', obtain_jwt_token),
+    re_path(r'^jwt-verify/', verify_jwt_token),
+    re_path(r'^jwt-refresh/', refresh_jwt_token),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from backend.user.serializers import SignupSerializer
+
+
+class SignupView(CreateAPIView):
+    model = get_user_model()
+    serializer_class = SignupSerializer
+    permission_classes = [
+        AllowAny,
+    ]
