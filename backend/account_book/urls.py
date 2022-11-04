4ef99939818b4deeb06de9fe7ddd2ec4fe3a496b
Jong012 <Jong012>
2022-11-05 04:35:22 +0900

    feat(account_book.urls): diary, memo url router 작성

--- backend/account_book/urls.py
+++ backend/account_book/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from account_book.views import AccountBookViewSet
+from account_book.views import AccountBookListCreateView, AccountBookDiaryViewSet, diary_restore, MemoViewSet
 
 router = routers.DefaultRouter()
-router.register('', AccountBookViewSet)
+router.register(r'(?P<account_book_id>\d+)/diary', AccountBookDiaryViewSet)
+router.register(r'(?P<account_book_id>\d+)/diary/(?P<account_book_diary_id>\d+)/memo', MemoViewSet)
+
 urlpatterns = [
+    path('', AccountBookListCreateView.as_view()),
     path('', include(router.urls)),
+    path('diary/<int:pk>/restore', diary_restore)
 ]
