9b9201f6c5629be476415c6c173f7809cfd65f6e
Jong012 <Jong012>
2022-11-04 23:42:13 +0900

    feat(account_book.urls): account-book urls router 생성

--- backend/account_book/urls.py
+++ backend/account_book/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from rest_framework import routers
 
+from account_book.views import AccountBookViewSet
+
+router = routers.DefaultRouter()
+router.register('', AccountBookViewSet)
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
