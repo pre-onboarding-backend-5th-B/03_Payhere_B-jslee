85ed7554ff2b936f311476db59c8fe7335f03c76
Jong012 <Jong012>
2022-11-04 14:54:33 +0900

    fix(user.views): serializer import 상대 경로로 수정.

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 
-from backend.user.serializers import SignupSerializer
+from .serializers import SignupSerializer
 
 
 class SignupView(CreateAPIView):
