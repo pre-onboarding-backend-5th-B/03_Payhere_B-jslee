f3fc8178f7da6eb623a963313c62842373aa7915
Jong012 <Jong012>
2022-11-04 16:14:46 +0900

    feat(user.urls): jwt urls 삭제

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path, re_path
-from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+from django.urls import path
 
 from .views import SignupView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
-    re_path(r'^jwt-auth/', obtain_jwt_token),
-    re_path(r'^jwt-verify/', verify_jwt_token),
-    re_path(r'^jwt-refresh/', refresh_jwt_token),
 ]
