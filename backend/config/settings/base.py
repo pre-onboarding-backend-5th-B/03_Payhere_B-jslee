6f1359d797017a64811ee4e5e34526951c764647
Jong012 <Jong012>
2022-11-05 04:29:21 +0900

    feat(config.settings): BasicAuthentication, SessionAuthentication 추가

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -135,6 +135,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
