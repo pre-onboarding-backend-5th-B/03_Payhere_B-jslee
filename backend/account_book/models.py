c371e60f197ca0dbda904c78456500b8d6042c04
Jong012 <Jong012>
2022-11-05 04:33:03 +0900

    feat(account_book.models): modeling 수정
    1. AccountBook
        - Meta 작성: db_table, ordering 작성
    
    2. AccountBookDiary 추가
        - 지출/소비 choices 작성
        - account_book fk field 작성
        - Meta 작성: db_table
    
    3. Memo
        - account_book_diary fk 추가

--- backend/account_book/models.py
+++ backend/account_book/models.py
@@ -1,21 +1,28 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 
 User = get_user_model()
 
 
 class AccountBook(models.Model):
-    class StatusChoices(models.TextChoices):
-        INCOME = "I", "수입"
-        EXPENDITURE = "E", "지출"
-
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(
         max_length=20,
-        validators=[MaxValueValidator(20)],
         help_text='가계부 이름을 지어 주세요.',
     )
+
+    class Meta:
+        db_table = 'account_book'
+        ordering = ['-id']
+
+
+class AccountBookDiary(models.Model):
+    class StatusChoices(models.TextChoices):
+        INCOME = 'I', '수입'
+        EXPENDITURE = 'E', '지출'
+
+    account_book = models.ForeignKey(AccountBook, on_delete=models.CASCADE)
     money = models.IntegerField(
         validators=[MinValueValidator(0)],
         help_text='금액을 적어 주세요.',
@@ -33,12 +40,13 @@ class AccountBook(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'account_book'
-        ordering = ['-id']
+        db_table = 'account_book_diary'
 
 
 class Memo(models.Model):
-    account_book = models.ForeignKey(AccountBook, on_delete=models.CASCADE)
+    account_book_diary = models.ForeignKey(AccountBookDiary,
+                                           on_delete=models.CASCADE,
+                                           related_name='memo')
     content = models.TextField(help_text='가계부 내용을 입력해주세요.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
